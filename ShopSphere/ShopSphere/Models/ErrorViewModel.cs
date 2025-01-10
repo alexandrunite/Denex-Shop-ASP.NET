5d4766826d8469ddd446c5c9f5ddd386fff6918d
denisamihaila <denisamihaila2000@yahoo.ro>
2025-01-10 19:34:36 +0200

    .

--- ShopSphere/ShopSphere/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShopSphere.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
